e4f0f92941b498899a81390fdee8737d5fa03b1b
jlchn <jianglichn@163.com>
2019-08-23 22:52:04 +0800

    Update scan_tcp_port.py

--- scan_tcp_port.py
+++ scan_tcp_port.py
@@ -14,16 +14,16 @@ start_time = datetime.now()
 
 for port in range(1, 1025):
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print "Opening Port: {}".format(port)
-        sock.close()
+  result = sock.connect_ex((remoteServerIP, port))
+  if result == 0:
+    print("Opening Port: {}".format(port))
+  sock.close()
 
 except KeyboardInterrupt:
     sys.exit()
 except socket.error:
-    print "Cannot connect to server"
+    print("Cannot connect to server")
     sys.exit()
 
 end_time = datetime.now()
-print ('Completed in: ', end_time - start_time)
+print('Completed in: ', end_time - start_time)
