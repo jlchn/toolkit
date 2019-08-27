f94af3f29a9de92c20a8a0fc3a4990d40035a4d0
jiangli <jianglichn@163.com>
2019-08-27 19:18:02 +0800

    add sourcelist updater for raspbian

---
+++ raspbian/update_sourcelist.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo echo "deb http://mirror.nus.edu.sg/raspbian/raspbian/ wheezy main contrib non-free rpi" >> /etc/apt/sources.list
+sudo echo "deb-src http://mirror.nus.edu.sg/raspbian/raspbian/ wheezy main contrib non-free rpi" >> /etc/apt/sources.list
+sudo echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ wheezy main contrib non-free rpi" >> /etc/apt/sources.list
+sudo echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ wheezy main contrib non-free rpi" >> /etc/apt/sources.list
+sudo echo "deb http://mirrors.neusoft.edu.cn/raspbian/raspbian/ wheezy main contrib non-free rpi" >> /etc/apt/sources.list
+sudo echo "deb-src http://mirrors.neusoft.edu.cn/raspbian/raspbian/ wheezy main contrib non-free rpi" >> /etc/apt/sources.list
+sudo echo "deb http://mirrors.ustc.edu.cn/raspbian/raspbian/ wheezy main contrib non-free rpi" >> /etc/apt/sources.list
+sudo echo "deb-src http://mirrors.ustc.edu.cn/raspbian/raspbian/ wheezy main contrib non-free rpi" >> /etc/apt/sources.list
